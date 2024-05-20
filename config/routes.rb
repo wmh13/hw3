105e7bea8f7d7d5ab8e9ecae522a3db0fb84a6f4
Michael Haynes <michael.haynes@kellogg.northwestern.edu>
2024-05-20 20:00:49 +0000

    stage1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get("/places", :controller => "places", :action => "index")
+  get("/entries", :controller => "entries", :action => "index")
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
